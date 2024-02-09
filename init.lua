2dba039ca498599d1e7acdf59daa560ef82ad64b
Rafael Koch Peres <rafaelkperes@gmail.com>
2024-02-09 20:06:24 -0300

    Center cursor and toggle line helpers

--- init.lua
+++ init.lua
@@ -396,6 +396,9 @@ vim.wo.number = true
 -- Relative line numbers
 vim.wo.relativenumber = true
 
+-- Cursor centralized in the screen
+vim.o.scrolloff = 999
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
@@ -444,6 +447,17 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Disable line annotations to allow raw-copying
+local function toggle_line_annotations()
+  -- sort of a "ternary" operator in lua
+  vim.o.signcolumn = vim.o.signcolumn == "yes" and "no" or "yes"
+  vim.o.relativenumber = not vim.o.relativenumber
+  vim.o.nu = not vim.o.nu
+  vim.cmd(':IBLToggle')
+end
+
+vim.keymap.set('n', '<leader>C', toggle_line_annotations, { desc = 'Toggle line annotations (allow raw copy)' })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
