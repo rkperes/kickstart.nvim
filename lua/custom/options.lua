72eb93115603c3c5d8a2708e489d0b16b682ebde
rkochp <rkochp@ext.uber.com>
2024-04-24 19:41:05 +0000

    [BREAK] Latest upstream and change to multifile
    Reset to latest upstream and reconfigure with multifile configuration.

---
+++ lua/custom/options.lua
@@ -0,0 +1,11 @@
+-- Relative line numbers
+vim.wo.relativenumber = true
+
+-- Cursor centralized in the screen
+vim.o.scrolloff = 999
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+-- Theme
+-- vim.cmd.colorscheme 'catppuccin'
