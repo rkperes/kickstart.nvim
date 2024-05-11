bebe479472dbb2af379cae169ccabb56373612c5
Rafael Koch Peres <rafaelkperes@gmail.com>
2024-05-11 12:20:52 -0300

    Enchance copilot config

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,5 +1,24 @@
 return {
   {
     'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = false,
+          debounce = 75,
+          keymap = {
+            accept = '<M-l>',
+            accept_word = false,
+            accept_line = '<M-L>',
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
+          },
+        },
+      }
+    end,
   },
 }
