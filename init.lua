3104a39e246564314783484f7eb2f5787e26e86c
Rafael Koch Peres <rafaelkperes@gmail.com>
2024-09-07 15:31:17 -0300

    Add sF (for hidden) and sG (for cwd)

--- init.lua
+++ init.lua
@@ -404,9 +404,22 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sF', function()
+        require('telescope.builtin').find_files {
+          hidden = true,
+          no_ignore = true,
+        }
+      end, { desc = '[S]earch [F]iles (include hidden and gitignored)' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sG', function()
+        local oil = require 'oil'
+        local cwd = oil.get_current_dir() or vim.fn.expand '%:p:h'
+        require('telescope.builtin').live_grep {
+          cwd = cwd,
+        }
+      end, { desc = '[S]earch by [G]rep in oil directory' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
