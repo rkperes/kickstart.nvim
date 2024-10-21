2b67cad5f34c2b3f366e9146fe3d0f1a420386b3
rkochp <rkochp@ext.uber.com>
2024-10-21 16:48:38 +0000

    Disable gopls, golangci-lint from Mason

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -74,23 +74,23 @@ require('lspconfig').gopls.setup {
 }
 
 -- go install github.com/nametake/golangci-lint-langserver@latest
-local lspconfig = require 'lspconfig'
-local configs = require 'lspconfig/configs'
-
-if not configs.golangcilsp then
-  configs.golangcilsp = {
-    default_config = {
-      cmd = { 'golangci-lint-langserver' },
-      root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
-      init_options = {
-        command = { 'golangci-lint', 'run', '--enable-all', '--disable', 'lll', '--out-format', 'json', '--issues-exit-code=1' },
-      },
-    },
-  }
-end
-lspconfig.golangci_lint_ls.setup {
-  filetypes = { 'go', 'gomod' },
-}
+-- local lspconfig = require 'lspconfig'
+-- local configs = require 'lspconfig/configs'
+--
+-- if not configs.golangcilsp then
+--   configs.golangcilsp = {
+--     default_config = {
+--       cmd = { 'golangci-lint-langserver' },
+--       root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+--       init_options = {
+--         command = { 'golangci-lint', 'run', '--enable-all', '--disable', 'lll', '--out-format', 'json', '--issues-exit-code=1' },
+--       },
+--     },
+--   }
+-- end
+-- lspconfig.golangci_lint_ls.setup {
+--   filetypes = { 'go', 'gomod' },
+-- }
 
 -- templ
 vim.filetype.add { extension = { templ = 'templ' } }
