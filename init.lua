04e79e22bb994f2330b84b4933ffa67b4a2fa291
rkochp <rkochp@ext.uber.com>
2026-05-18 13:08:22 +0000

    Change format keybind

--- init.lua
+++ init.lua
@@ -718,7 +718,7 @@ do
     },
   }
 
-  vim.keymap.set({ 'n', 'v' }, '<leader>f', function() require('conform').format { async = true } end, { desc = '[F]ormat buffer' })
+  vim.keymap.set({ 'n', 'v' }, '<leader>F', function() require('conform').format { async = true } end, { desc = '[F]ormat buffer' })
 end
 
 -- ============================================================
